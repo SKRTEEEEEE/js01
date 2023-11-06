eb8c91cfbc823c1f58fce5a5370c31ec1375b27d
Adan <adanreh.m@gmail.com>
2023-11-06 13:20:44 +0100

    Hola mundo

---
+++ js01.js
@@ -0,0 +1,17 @@
+numero1 = 10;
+// document.getElementById('js01').innerText = numero1;
+console.log(numero1);
+
+//OPERADORES tenemos suma +, resta -, division /, multiplicacion *, modulo %,  exponente **
+//STRINGS
+let texto;
+texto = 'Hola Mundo';
+console.log('el texto tiene', texto.length, 'palabras');
+console.log(texto.toUpperCase());
+console.log(texto.toLowerCase());
+
+//CONCATENACION (*=, -=, *=, /=)
+let num5 = 5;
+// num5 = num5+4
+num5 += 4;
+console.log(num5);
